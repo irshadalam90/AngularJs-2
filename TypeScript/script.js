7a6c3fe25184d5a64eb9c4219d35671c75863dce
irshadalam90 <Deepti.sharma@berylsystems.com>
2017-01-28 20:31:17 +0530

    added angularJs 2 project

---
+++ TypeScript/script.js
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Irshad Alam";
+document.body.innerHTML = greeter(user);
+var arr = [10, 20, 30, 40];
---
+++ TypeScript/script.ts
@@ -0,0 +1,10 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+var user = "Irshad Alam";
+
+document.body.innerHTML = greeter(user);
+
+var arr:number[] = [10,20,30,40];
+console.log(arr[0]);
